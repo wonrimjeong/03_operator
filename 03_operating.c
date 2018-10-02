30fedacd032685c0506d976f733085f7d660ccb7
wonrimjeong <wonrim0802@gmail.com>
2018-10-02 16:26:34 +0900

    03_average

--- 03_operating.c
+++ 03_operating.c
@@ -5,12 +5,12 @@
 
 int main(int argc, char *argv[]) {
 
-	int sec;
+	int in1, in2, in3;
 	
-	printf("input :");
-	scanf("%d", &sec);
+	printf("input 3 integers :");
+	scanf("%d %d %d", &in1, &in2, &in3);
 	
-	printf("sec. %i is %i : %i : %i\n" ,sec, sec/3600, (sec%3600)/60, sec%60);
+	printf("average of %i %i %i is %f\n" , in1, in2, in3, (double)(in1+in2+in3)/3);
 	
 	return 0;
 	
