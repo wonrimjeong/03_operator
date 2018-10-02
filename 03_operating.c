464231f0f11314a63f5db2c18853e0a6273fa8e7
wonrimjeong <wonrim0802@gmail.com>
2018-10-02 16:22:27 +0900

    03_sec

--- 03_operating.c
+++ 03_operating.c
@@ -5,16 +5,13 @@
 
 int main(int argc, char *argv[]) {
 
-	int year;
-	int result;
+	int sec;
 	
 	printf("input :");
-	scanf("%d", &year);
+	scanf("%d", &sec);
 	
-	result = (year%4 == 0 && year%100 != 0) || (year%400 == 0);
+	printf("sec. %i is %i : %i : %i\n" ,sec, sec/3600, (sec%3600)/60, sec%60);
 	
-	printf("leap year? %d\n", result);
-
 	return 0;
 	
 	
